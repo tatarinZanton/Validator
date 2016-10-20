3b87172915bc328b93bef30204a8a4cc620cbeda
tatarinZanton <tatarin.com@gmail.com>
2016-10-20 18:43:01 +0300

    new structer need test

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 'use strict';
+$(document).ready(function(){
+	_("#auth").init({
 
-_("#idForm").init({
-	"class":{
-		"login":"login",
-		"passwd" : "passwd"
-	},
-	"name":{
-		"e-mail":"email"
-	}
-});
+		configure:{
+					"login" : ".checkLog",
+					"passwd": "[name=passwd]"	
+					},
+		confLength:{
+					"default" : [-4, 12],
+					"passwd"  : [8, 32],
+					// "asdfdsdf": [3,5]
+		},
+		classes:{"error"  : "errValid",
+				 "success": "sucValid"},
+
+		messages:{errEmpty : "Поле не заполнено",
+				  errLengthMax: "Длинна поля больше максимальной",
+				  errLengthMin: "Длинна поля меньше минимальной",
+				  errLogin : "Ошибка логина",
+				  errPasswd : "Ошибка пароля",
+				  errEmail : "Ошибка email",
+				  errTel : "Ошибка телефона",
+				  errText : "Ошибка текста",
+				}
+	});
+
+})
+
+
--- validator.js
+++ validator.js
@@ -1,23 +1,213 @@
 "use strict"
 ;(function(){
 
+	function checkLengthValues(confLength){
+		var result = true;
+		for (var rule in confLength) {
+			switch(rule){
+	  			case "default":
+	  				if(confLength.default[0] < 0 || confLength.default[1] < 0 ) {
+	  					console.log("Wrong length value of " + rule);
+	  					result = false;
+	  				} 
+					break;
+	  			case "login":
+	  				if(confLength.login[0] < 0 || confLength.login[1] < 0 ) {
+	  					console.log("Wrong length value of " + rule);
+	  					result = false;
+	  				}
+					break;
+				case "passwd":
+					if(confLength.passwd[0] < 0 || confLength.passwd[1] < 0 ){ 
+						console.log("Wrong length value of " + rule);
+						result = false;
+					}
+					break;
+				case "tel":
+					if(confLength.tel[0] < 0 || confLength.tel[1] < 0 ){ 
+						console.log("Wrong length value of " + rule);
+						result = false;
+					}
+					break;
+				case "email":
+					if(confLength.email[0] < 0 || confLength.email[1] < 0 ){ 
+						console.log("Wrong length value of " + rule);
+						result = false;
+					}
+					break;
+				case "text":
+					if(confLength.text[0] < 0 || confLength.text[1] < 0 ){ 
+						console.log("Wrong length value of " + rule); 
+						result = false;
+					}
+					break;
+			}
+		}
+		if (result) {
+			return true;
+		}
+		return false;
+	}
+
+	var self;
+
 	function Validator(form){
 		this.form = $(form);
+		self = this;
+		this.min = 6;
+		this.max = 25;
+		this.configure = undefined;
+
+		// Сообщения по-умолчанию
+		this.errEmpty = "Поле не заполнено";
+		this.errLengthMax= "Длинна поля больше максимальной";
+		this.errLengthMin= "Длинна поля меньше минимальной";
+		this.errLogin = "Ошибка логина";
+		this.errPasswd = "Ошибка пароля";
+		this.errEmail = "Ошибка email";
+		this.errTel = "Ошибка телефона";
+		this.errText = "Ошибка текста";
+
+		// классы по умолчанию
+		this.errorClass   = "errValid";
+		this.successClass = "sucValid";
 	}
 
-	Validator.prototype.init = function(configure, class, messages){
-		if (messages === undefined) {
-			messages = {};
+	Validator.prototype.init = function(data){
+		this.configure = data.configure;
+
+		var classes  = data.classes,
+			messages = data.messages,
+	  		confLength = data.confLength;
+
+	  	if (checkLengthValues(confLength)) {
+		  	for (var rule in confLength) {
+		  		switch(rule){
+		  			case "default":
+		  				this.min = confLength.default[0];
+		  				this.max = confLength.default[1];
+						break;
+		  			case "login":
+						this.minLoginLength = confLength.login[0];
+			  			this.maxLoginLength = confLength.login[1];
+						break;
+					case "passwd":
+						this.minPassLength = confLength.passwd[0];
+			  			this.maxPassLength = confLength.passwd[1];
+						break;
+					case "tel":
+						this.minTelLength = confLength.tel[0];
+			  			this.maxTelLength = confLength.tel[1];
+						break;
+					case "email":
+						this.minEmailLength = confLength.email[0];
+			  			this.maxEmailLength = confLength.email[1];
+						break;
+					case "text":
+						this.minTextLength = confLength.text[0];
+			  			this.maxTextLength = confLength.text[1];
+						break;
+					default:
+						console.log("Wrong type of length");
+						return;
+					break;
+		  		}
+		  	}
+	  	}
+
+		for (var rule in this.configure) {
+			switch(rule){
+				case "login":
+					$(this.configure.login).on("blur", function(){
+						self.checkLogin($(this));
+					})
+					break;
+				case "passwd":
+					$(this.configure.passwd).on("blur", function(){
+						self.checkPasswd($(this));
+					})
+					break;
+				case "tel":
+					$(this.configure.tel).on("blur", function(){
+						self.checkTel($(this));
+					})
+					break;
+				case "email":
+					$(this.configure.email).on("blur", function(){
+						self.checkEmail($(this));
+					})
+					break;
+				case "text":
+					$(this.configure.text).on("blur", function(){
+						self.checkText($(this));
+					})
+					break;
+				default:
+					console.log("Wrong type of validation");
+					return;
+				break;
+			}
 		}
+
+		this.form.on("submit", function(event){
+			var result = true;
+			for (var rule in self.configure) {
+				switch(rule){
+					case "login":
+						if (!self.checkLogin($(self.configure.login))) {
+							result = false;
+						}
+						break;
+					case "passwd":
+						if (!self.checkPasswd($(self.configure.passwd))) {
+							result = false;
+						}
+						break;
+					case "tel":
+						if (!self.checkTel($(self.configure.tel))) {
+							result = false;
+						}
+						break;
+					case "email":
+						if (!self.checkEmail($(self.configure.email))) {
+							result = false;
+						}
+						break;
+					case "text":
+						if (!self.checkText($(self.configure.text))) {
+							result = false;
+						}
+						break;
+				}
+			}
+			if (!result) {
+				event.preventDefault();	
+			}
+		})
 	}
 	
-	Validator.prototype.checkLogin = function(){}
-	Validator.prototype.checkPasswd = function(){}
-	Validator.prototype.checkEmail = function(){}
-	Validator.prototype.checkTel = function(){}
-	Validator.prototype.checkText = function(){}
+	Validator.prototype.checkLogin = function(elem){
 
-	Validator.prototype.checkEmpty = function(){}
+		if(self.checkEmpty(elem)){
+			
+			return true;
+		}
+		
+		return false;
+		
+	} // login
+	Validator.prototype.checkPasswd = function(){} // passwd
+	Validator.prototype.checkEmail = function(){} // email
+	Validator.prototype.checkTel = function(){} //tel
+	Validator.prototype.checkText = function(){} // text
+
+	Validator.prototype.checkEmpty = function(elem){
+		if (elem.val() === "") {
+			elem.addClass(self.errorClass);
+			return false;
+		}
+		return true;
+	}
 	Validator.prototype.checkLength = function(){}
 
 	function make(form){
@@ -27,5 +217,4 @@
 
 	window._ = make;
 
-	
 })();
